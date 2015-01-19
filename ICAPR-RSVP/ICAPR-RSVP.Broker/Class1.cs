4555b63da7260051ad55bc6ffe192b433e6ef25f
danielgg88 <daniel.gg88@gmail.com>
2015-01-19 13:34:06 +0100

    Initial setup

---
+++ ICAPR-RSVP/ICAPR-RSVP.Broker/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICAPR_RSVP.Broker
+{
+    public class Class1
+    {
+    }
+}
---
+++ ICAPR-RSVP/ICAPR-RSVP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICAPR_RSVP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
