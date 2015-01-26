bc9b2c49890e561444f738850b809937c33a24cb
danielgg88 <daniel.gg88@gmail.com>
2015-01-26 18:24:27 +0100

    Broker test files added

---
+++ ICAPR-RSVP/ICAPR-RSVP.Test/MockupImplementations/PortBlockingInputTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using ICAPR_RSVP.Broker;
+using ICAPR_RSVP.Misc;
+
+
+namespace ICAPR_RSVP.Test.MockupImplementations
+{
+    //Test simulating EyeTribe data input
+    public class PortBlockingInputTest : PortBlocking
+    {
+        private Thread _workerThread;
+        private bool _isRunning;
+
+        #region Properties
+        public override bool IsRunning { get { return _isRunning; } }
+        #endregion
+
+        public PortBlockingInputTest()
+            : base()
+        {
+            _workerThread = new Thread(DoWork);
+        }
+
+        #region Protected methods
+        protected override void OnStart()
+        {
+            _workerThread.Start();
+            this._isRunning = true;
+        }
+
+        protected override void OnStop()
+        {
+            _workerThread.Join();
+            this._isRunning = false;
+        }
+        #endregion
+
+        private void DoWork()
+        {
+            //Main testing method
+            Random rnd = new Random();
+            int timestamp = 0;
+            Misc.Eyes eyes;
+            Misc.Eye eye = new Eye();
+            eye.PupilSize = 5;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                timestamp += rnd.Next(1, 1000);
+                eyes = new Eyes(timestamp, eye, eye);
+                base.PushItem(new Bundle<Eyes>(ItemTypes.Eyes, eyes));
+            }
+        }
+    }
+}
