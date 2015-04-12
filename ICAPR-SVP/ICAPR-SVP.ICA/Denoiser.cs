e20df90895bb85a0931df8397e42f73daeee7472
Ioannis Sintos <sintosioannis@outlook.com>
2015-04-12 10:23:29 +0200

    Added denoiser interface

--- ICAPR-SVP/ICAPR-SVP.ICA/Denoiser.cs
+++ ICAPR-SVP/ICAPR-SVP.ICA/Denoiser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ICAPR_SVP.ICA
 {
-    public class Denoiser
+    public class Denoiser : IDenoiser
     {
         private MLApp.MLApp matlab;
 
---
+++ ICAPR-SVP/ICAPR-SVP.ICA/IDenoiser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICAPR_SVP.Misc;
+
+namespace ICAPR_SVP.ICA
+{
+    public interface IDenoiser{
+        double[][] denoiseEyes(Queue<Eyes> eyes_queue,double[][] eyes_array);
+    }
+}
